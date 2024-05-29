2bacfe067d3a43fcdad69f79749b24fb6a62819c
James <jamesonley@outlook.com>
2024-05-28 22:13:44 -0400

    Chapter 5 Structs

--- src/main.rs
+++ src/main.rs
@@ -237,16 +237,10 @@ fn main() {
 
         // A slice references a sequence of elements in a collection. It does not have ownership. 
 
-        fn first_words(s:&String) -> usize {
-            let bytes = s.as_bytes();
-
-            for (i, &item) in bytes.iter().enumerate(){
-                if item == b' ' {
-                    return i;
-                }
-            }
-            s.len()
-        }
+        let s = String::from("hello world");
+        let hello = &s[..5];
+        let world = &s[6..];
+        println!("{}:{}", hello, world);
 
         
 }
